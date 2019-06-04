40f5f66d4bce626ee37268c0f2f2b23de2af99bd
Alla Goichuk <a.goichuk@gmail.com>
2019-06-04 21:48:41 +0100

    adding docker-compose

---
+++ tests/integration-tests.sh
@@ -0,0 +1,37 @@
+curl -s -X POST $PAYMENTS_URL \
+    -H "Content-Type: application/json" \
+    -d "{ 'amount': 0, 
+          'currency': 'string', 
+          'cardNumber': 'string', 
+          'expiryDate': 'string', 
+          'cvv': 'string', 
+          'idempotencyKey': 'string' }" && echo
+
+curl -s -X POST $PAYMENTS_URL \
+    -H "Content-Type: application/json" \
+    -d "{ 'amount': 10, 
+          'currency': 'string', 
+          'cardNumber': 'string', 
+          'expiryDate': 'string', 
+          'cvv': 'string', 
+          'idempotencyKey': 'string' }" && echo
+
+curl -s -X POST $PAYMENTS_URL \
+    -H "Content-Type: application/json" \
+    -d "{ 'amount': 10, 
+          'currency': 'USD', 
+          'cardNumber': '$MockCardNumberDeclined', 
+          'expiryDate': '08/12', 
+          'cvv': '123', 
+          'idempotencyKey': 'string' }" && echo
+
+curl -s -X POST $PAYMENTS_URL \
+    -H "Content-Type: application/json" \
+    -d "{ 'amount': 10, 
+          'currency': 'USD', 
+          'cardNumber': '4111111111111100', 
+          'expiryDate': '08/12', 
+          'cvv': '123', 
+          'idempotencyKey': 'string' }" && echo
+
+exit 0
