5473c5651360c52b18d4bad558d4bf7007d58652
Alla Goichuk <a.goichuk@gmail.com>
2019-06-04 21:50:37 +0100

    Not implemented exception for RestBankClient

--- Checkout.Payments.Processor/Services/RestBankClient.cs
+++ Checkout.Payments.Processor/Services/RestBankClient.cs
@@ -8,26 +8,9 @@ namespace Checkout.Payments.Processor.Services
 {
     public class RestBankClient : IBankClient
     {
-        private const string FAILED_CARD_NUMBER = "4111111111111112";
-
         public Task<PaymentProcessingResults> InitiatePayment(Models.Payment payment)
         {
-            if (payment.CardNumber == FAILED_CARD_NUMBER)
-            {
-                return Task.FromResult(new PaymentProcessingResults
-                {
-                    PaymentStatus = PaymentStatus.Declined,
-                    Error = PaymentProcessingErrorCodes.no_credit,
-                    BankIdentifier = Guid.NewGuid().ToString()
-                });
-            }
-
-            return Task.FromResult(new PaymentProcessingResults
-            {
-                PaymentStatus = PaymentStatus.Authorized,
-                Error = PaymentProcessingErrorCodes.none,
-                BankIdentifier = Guid.NewGuid().ToString()
-            });
+            throw new NotImplementedException();
         }
 
     }
